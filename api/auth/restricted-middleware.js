cde320d4febe1a931f2db7aabd52ce34140e51d7
Tim Shaker <tim@tmshkr.com>
2020-06-19 16:31:04 -0700

    update restricted-middleware

--- api/auth/restricted-middleware.js
+++ api/auth/restricted-middleware.js
@@ -3,21 +3,13 @@ const secrets = require("../../config/secrets.js");
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    if (token) {
-      jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
-        if (err) {
-          res.status(401).json({ you: "can't touch this" });
-        } else {
-          req.decodedJwt = decodedToken;
-          console.log(req.decodedJwt);
-          next();
-        }
-      });
-    } else {
-      throw new Error("invalid auth data");
-    }
+    const authHeader = req.headers.authorization || "";
+    const token = authHeader.match(/Bearer (.+)/)[1];
+
+    req.jwt = jwt.verify(token, secrets.jwtSecret);
+    console.log(req.jwt);
+    next();
   } catch (err) {
-    res.status(401).json({ error: err.message });
+    next({ code: 401, message: "Unauthorized" });
   }
 };
