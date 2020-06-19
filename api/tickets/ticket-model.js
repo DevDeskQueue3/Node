985aa90596885d53629ddd81969b949b303358a4
Tim Shaker <tim@tmshkr.com>
2020-06-19 16:14:21 -0700

    reorganize api files

---
+++ api/tickets/ticket-model.js
@@ -0,0 +1,28 @@
+const db = require("../../dbConfig.js");
+
+// Return list of all tickets
+function find() {
+  return db("tickets");
+}
+
+// Add new ticket to database
+function add(ticket) {
+  return db("tickets").insert(ticket).returning("*");
+}
+
+// Removes ticket selected by id
+function remove(id) {
+  return db("tickets").where({ id }).delete();
+}
+
+// Updates ticket by id
+function update(ticket, id) {
+  return db("tickets").where({ id }).update(ticket);
+}
+
+module.exports = {
+  find,
+  add,
+  remove,
+  update,
+};
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 const server = express();
-const authRouter = require('./auth/auth-router.js');
-const ticketRouter = require('./tickets/tickets-router.js');
+const authRouter = require("./api/auth/auth-router.js");
+const ticketRouter = require("./api/tickets/tickets-router.js");
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/auth', authRouter);
-server.use('/api/tickets', ticketRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/tickets", ticketRouter);
 
-server.get('/', (req, res) => res.json({ server: 'up' }));
+server.get("/", (req, res) => res.json({ server: "up" }));
 server.use(errorHandler);
 
 module.exports = server;
 
 function errorHandler(err, req, res, next) {
-	res.status(err.code).json({ message: err.message });
+  res.status(err.code).json({ message: err.message });
 }
