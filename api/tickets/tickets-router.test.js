d03f44f36bcf3236736f4c0c308533626451bbb7
Ewa Czech <ewa.czech@hotmail.com>
2020-06-24 23:49:27 +0200

    add tests for GET /tickets endpoint

--- api/tickets/tickets-router.test.js
+++ api/tickets/tickets-router.test.js
@@ -1,9 +1,26 @@
 const request = require("supertest");
 const server = require("../../server.js");
-const db = require("../../dbConfig");
 
-describe("setup test", () => {
-  it("runs", () => {
-    expect(true).toBe(true);
+let token;
+
+describe("GET /tickets", () => {
+  it("it should require authorization", async () => {
+    const tickets = await request(server).get("/api/tickets");
+    expect(tickets.status).toBe(401);
+  });
+
+  it("it should require authorization", async () => {
+    const credentials = await request(server)
+      .post("/api/auth/login")
+      .send({ email: "testuser1@mail.com", password: "12345" })
+      .expect(200);
+
+    token = credentials.body.token;
+
+    const tickets = await request(server)
+      .get("/api/tickets")
+      .set("Authorization", "Bearer " + token);
+
+    expect(tickets.status).toBe(200);
   });
 });
