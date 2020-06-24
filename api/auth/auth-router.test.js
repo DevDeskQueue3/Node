812b1df1f79ea651af690c8f57e5bc7f865057fe
Tim Shaker <tim@tmshkr.com>
2020-06-23 20:02:28 -0700

    POST /api/auth/register returns 201

--- api/auth/auth-router.test.js
+++ api/auth/auth-router.test.js
@@ -2,8 +2,26 @@ const request = require("supertest");
 const server = require("../../server.js");
 const db = require("../../dbConfig");
 
+beforeAll(() => db("users").del());
+
 describe("setup test", () => {
   it("runs", () => {
     expect(true).toBe(true);
+  });
+});
+
+describe("POST /api/auth/register", () => {
+  it("returns 201", () => {
+    return request(server)
+      .post("/api/auth/register")
+      .send({
+        name: "Alice",
+        email: "alice@gmail.com",
+        password: "hello",
+        role: "STUDENT",
+      })
+      .then((res) => {
+        expect(res.status).toBe(201);
+      });
   });
 });
