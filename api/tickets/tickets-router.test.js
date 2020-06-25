885a790d00c4ece19bc5f737b4565e296a46b894
Tim Shaker <tim@tmshkr.com>
2020-06-24 19:53:25 -0700

    update tickets-router.test.js

--- api/tickets/tickets-router.test.js
+++ api/tickets/tickets-router.test.js
@@ -3,6 +3,16 @@ const server = require("../../server.js");
 const db = require("../../dbConfig");
 
 let token;
+beforeAll(async () => {
+  await db.seed.run();
+  const credentials = await request(server)
+    .post("/api/auth/login")
+    .send({ email: "testuser1@mail.com", password: "12345" })
+    .expect(200);
+
+  token = credentials.body.token;
+  console.log(token);
+});
 
 describe("GET /tickets", function () {
   it("it should require authorization", async () => {
@@ -17,6 +27,7 @@ describe("GET /tickets", function () {
       .expect(200);
 
     token = credentials.body.token;
+    // console.log(token);
 
     const tickets = await request(server)
       .get("/api/tickets")
@@ -29,9 +40,17 @@ describe("GET /tickets", function () {
 describe("addddddd", () => {
   it("whwhwhwhhwhw", () => {
     return request(server)
-      .post("api/tickets")
+      .post("/api/tickets")
+      .set("Authorization", "Bearer " + token)
+      .send({
+        title: "test",
+        description: "testing",
+        what_ive_tried: "testing",
+      })
       .then((res) => {
-        expect(res.type).toBe("text/html");
+        console.log(res.body);
+        expect(res.type).toBe("application/json");
+        expect(res.status).toBe(201);
       });
   });
 });
