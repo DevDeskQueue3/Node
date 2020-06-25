2e3a895d58d9768b6ceb363319952c0ab97fab7e
Ewa Czech <ewa.czech@hotmail.com>
2020-06-25 04:30:27 +0200

    add post endpoint error

--- api/tickets/tickets-router.test.js
+++ api/tickets/tickets-router.test.js
@@ -1,9 +1,10 @@
 const request = require("supertest");
 const server = require("../../server.js");
+const db = require("../../dbConfig");
 
 let token;
 
-describe("GET /tickets", () => {
+describe("GET /tickets", function () {
   it("it should require authorization", async () => {
     const tickets = await request(server).get("/api/tickets");
     expect(tickets.status).toBe(401);
@@ -22,5 +23,15 @@ describe("GET /tickets", () => {
       .set("Authorization", "Bearer " + token);
 
     expect(tickets.status).toBe(200);
+  });
+});
+
+describe("addddddd", () => {
+  it("whwhwhwhhwhw", () => {
+    return request(server)
+      .post("api/tickets")
+      .then((res) => {
+        expect(res.type).toBe("text/html");
+      });
   });
 });
